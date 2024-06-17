63cd797927eb7bcabe188eb4cc3f60e74d0ce041
nc-ellie-judge <ellie.judge@northcoders.com>
2024-06-17 14:34:43 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 app.use(express.json());
 const { getCommentsByArticleId, postNewComment, getArticleById, getArticles, patchArticle, getEndpoints, getTopics, deleteCommentByCommentId, getUsers } = require("./controllers");
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors, handle404NotFound, } = require("./errors/index.js");
+const cors = require('cors');
 
+app.use(cors());
 app.get("/api/topics", getTopics);
 app.get("/api", getEndpoints);
 app.get("/api/articles/:article_id", getArticleById);
